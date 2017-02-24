b9f2f9cf6bde03456fcd95da5471aaa688d51f6a
Roberto Confalonieri <roberto.confalonieri@gmail.com>
2017-02-24 17:42:45 +0100

    Creacion del proyecto

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author rconfalonieri
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenidos a la segunda clase de M05");
+		ArrayList<String> listaNombre;
+	}
+
+}
